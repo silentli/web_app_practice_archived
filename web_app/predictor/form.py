5e348c0e8c0160f5d08ed0b2c7cbc5e5ccc84787
silentli <heylibai@gmail.com>
2024-12-21 10:18:28 +0100

    feat: add form

---
+++ web_app/predictor/form.py
@@ -0,0 +1,25 @@
+from django import forms
+
+class PredictionForm(forms.Form):
+    Date = forms.DateField(
+        widget=forms.TextInput(attrs={'placeholder': 'dd/mm/yyyy'}),
+        input_formats=['%d/%m/%Y'],
+        required=True
+    )
+    Hour = forms.IntegerField(
+        min_value=0, max_value=23,
+        widget=forms.NumberInput(attrs={'placeholder': '0-23'}),
+        required=True
+    )
+    Holiday = forms.TypedChoiceField(
+        choices=[(1, 'Yes'), (0, 'No')],
+        widget=forms.Select(),
+        coerce=int,
+        required=True
+    )
+    Rain = forms.TypedChoiceField(
+        choices=[(1, 'Yes'), (0, 'No')],
+        widget=forms.Select(),
+        coerce=int,
+        required=True
+    )
